427de2d7970987299076fb088c7e80d2e5a45908
SidraOsman <osmansidra4@gmail.com>
2023-10-04 01:01:34 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+let mondayWork = function(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(style="*"){
+    return function(adjective = "special"){
+        return `You are ${style}${adjective}${style}!`
+    }
+}
+
+ function saturdayFun(activity="roller-skate" , activity2="bathe my dog"){
+    return `This Saturday, i want to ${activity} && ${activity2}!`
+}
